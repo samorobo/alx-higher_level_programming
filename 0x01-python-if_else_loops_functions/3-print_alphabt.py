e6d07a71c384e6d8a239dbe98c617e4823bbd1d3
‘samorobo’ <samgodwin111@gmail.com>
2022-06-28 12:14:12 -0700

    My third commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{:c}".format(i), end='')
+i = 0
+while i < 26: 
+    if i != 4 and i != 16:   
+        print('{:c}'. format(i + 97), end='')
+    i = i + 1
