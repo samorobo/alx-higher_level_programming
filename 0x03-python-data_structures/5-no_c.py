5d9bf57b987302fb379369d094aa9004877ae14c
‘samorobo’ <samgodwin111@gmail.com>
2022-07-04 09:02:20 -0700

    My fifth commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
-    if i in my_list:
+    for i in my_string:
         if i != 'c' and i != 'C':
-            new_str += i
-            return new_str
+            new_str = new_str + i
+    return new_str
