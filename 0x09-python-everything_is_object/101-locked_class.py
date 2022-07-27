03f45908d980c387870bfaf27ad842c908edc7a0
‘samorobo’ <samgodwin111@gmail.com>
2022-07-27 09:51:32 -0700

    MY Advanced task commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" LockedClass
-"""
-
-
 class LockedClass:
-    __slots__= ['first_name']
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
