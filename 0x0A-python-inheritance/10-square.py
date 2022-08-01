2c594fd05ded8c53fa3d5cf3ad9097d5bd8fbe14
‘samorobo’ <samgodwin111@gmail.com>
2022-08-01 15:26:32 -0700

    My Overall commit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,18 +1,17 @@
-
-
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square."""
+'''
+BaseGeometry module
+'''
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    ''' class Square '''
     def __init__(self, size):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
+        ''' Argument:
+            @size: size of the square
+        '''
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(self.__size, self.__size)
