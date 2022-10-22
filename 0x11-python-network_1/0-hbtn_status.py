61982bdfc7e18855a4c6889e804732f7f54087e5
samorobo <samgodwin111@gmail.com>
2022-10-21 19:34:58 -0700

    My overall commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """A script that
 - fetches https://alx-intranet.hbtn.io/status.
 - uses urlib package
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,12 +5,13 @@
 - displays the body of the response (decoded in utf-8).
 """
 
+
 if __name__ == "__main__":
     import sys
     from urllib import request, error
 
     try:
-        with request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode('UTF-8'))
-    except error.HTTPError as e:
-        print('Error code:', e.code)
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
